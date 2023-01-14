be5073bc2d5f9f05e67c311ca6f044213d77fa79
DevJ <devresearcher24@gmail.com>
2023-01-14 22:33:34 +0200

    quiz

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,27 @@
+
+export const Questions = [
+    {
+        prompt: "What is 1 + 3",
+        OptionA: "24",
+        optionB: "4",
+        optionC: "2",
+        optionD: "13",
+        answer: "B",
+    },
+    {
+        prompt: "What is 10 + 30",
+        OptionA: "24",
+        optionB: "40",
+        optionC: "2",
+        optionD: "13",
+        answer: "B",
+    },
+    {
+        prompt: "What is 41 + 3",
+        OptionA: "24",
+        optionB: "4",
+        optionC: "2",
+        optionD: "44",
+        answer: "D",
+    },
+];
